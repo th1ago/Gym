f30e310d37c1d7f0f6d99967edee2af3349d4ba2
Thiago <thiago_murakami@hotmail.com>
2020-12-28 23:15:13 -0300

    project gym

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+// motor de view - tudo que for html
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res) {
+    const index = {
+        avatar_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT02hPpuOZnTphNKn3Fws3iug8jU-RgpC5AJw&usqp=CAU",
+        name: "Vegeta ",
+        role: "Principe do planeta Vegeta",
+        description: "Personagem que demonstra seu poder através de bastante treinamentos e esforcos",
+        link: [
+            {name: "Github", url: "https://github.com/th1ago/"},
+            {name: "Facebook", url: "https://github.com/th1ago/"},
+            {name: "Linkedin", url: "https://github.com/th1ago/"}
+        ]
+    }
+
+    return res.render("index", {index})
+})
+
+server.get("/anime", function(req, res) {
+    return res.render("anime", {items: videos})
+})
+
+server.get("/video", function(req, res) {
+    const id = req.query.id
+
+    const video = videos.find(function(video) {
+        return video.id == id        
+    })
+
+    if (!video) {
+        return res.send("video not found")
+    }
+
+    return res.render("video", {item: video})
+
+})
+
+server.listen(5000, function() {
+    console.log("server run")
+})
