f7cdd71360c079bfbbbcaf9d962e507f5ffc816d
Thiago <thiago_murakami@hotmail.com>
2021-02-11 23:26:42 -0300

    use the node fs

--- instructores.js
+++ instructores.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
 
@@ -6,5 +8,12 @@ exports.post = function(req, res) {
             return res.send('Preencha os campos')
         }
     }
+
+    fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
+        if (err) return res.send("Write file error")
+
+        return res.redirect("/instructores")
+    })
+
     return res.send(req.body)
 }
