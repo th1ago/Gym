b4438330b191af414d47206da09c594d25b71d06
Thiago <thiago_murakami@hotmail.com>
2021-02-11 23:37:05 -0300

    work with data

--- instructores.js
+++ instructores.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require("./data.json")
 
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -9,11 +10,14 @@ exports.post = function(req, res) {
         }
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
+    // add body para o array
+    data.instrutor.push(req.body);
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if (err) return res.send("Write file error")
 
-        return res.redirect("/instructores")
+        return res.redirect("/instrutores")
     })
 
-    return res.send(req.body)
+    // return res.send(req.body)
 }
