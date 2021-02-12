5cfde68588fef930ef2995c282e2abc698557865
Thiago <thiago_murakami@hotmail.com>
2021-02-12 18:33:14 -0300

    fixing the data structure

--- instructores.js
+++ instructores.js
@@ -10,8 +10,22 @@ exports.post = function(req, res) {
         }
     }
 
+    let {avatar_url, name, niver, geneder, services} = req.body
+
+    niver = Date.parse(niver);
+    const created_at = Date.now();
+    const id = Number(data.instrutor.length + 1);
+
     // add body para o array
-    data.instrutor.push(req.body);
+    data.instrutor.push({
+        avatar_url,
+        name, 
+        niver, 
+        geneder, 
+        services,
+        created_at,
+        id
+    });
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if (err) return res.send("Write file error")
