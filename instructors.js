9deed5eab0f2564813d4efef74dd4e54967337a0
Thiago <thiago_murakami@hotmail.com>
2021-03-14 23:25:09 -0300

    Put - HTTP

--- instructors.js
+++ instructors.js
@@ -77,5 +77,29 @@ exports.edit = function(req, res) {
 }
 
 exports.put = function(req, res) {
+    const {id} = req.body
+    let index = 0
 
+    const foundInstructor = data.instrutor.find(function(instrutor, foundIndex) {
+        if (id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+    
+    if(!foundInstructor) return res.send("Not found")
+
+    const instructor =  {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)   
+    }
+
+    date.instructor[index]= instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write error")
+
+        return res.redirect(`/instructors/${id}`)
+    })
 }
